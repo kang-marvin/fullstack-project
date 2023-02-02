16b68b2d6d8ee14a86c70cee45d63b9d5dd53e86
Marvin Kang'ethe <marvinjameskang@gmail.com>
2023-02-02 18:37:14 +0300

    [Frontend] Create the list panel that shows cat images

--- frontend/src/components/dashboard.js
+++ frontend/src/components/dashboard.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { Grid, Input, Button } from "semantic-ui-react";
+import { Grid, Input, Button, Card, Image } from "semantic-ui-react";
+
 
 const SearchPanel = props => {
   const {
@@ -36,8 +38,67 @@ const SearchPanel = props => {
   )
 }
 
+const ListImages = props => {
+  const { results, favorites, updateFavorites } = props;
+
+  const addToFavorites = (identifier) => {
+    const list = [...favorites, identifier]
+    updateFavorites(list)
+  }
+
+  const removeFromFavorites = (identifier) => {
+    const list = favorites.filter(element => element !== identifier)
+    updateFavorites(list)
+  }
+
+  const AddToFavorites = props => {
+    const { identifier } = props;
+    return <Button fluid color="green" onClick={() => addToFavorites(identifier)}>Add To Favorites</Button>
+  }
+
+  const RemoveFromFavorites = props => {
+    const { identifier } = props;
+    return <Button fluid color="red" onClick={() => removeFromFavorites(identifier)}>Remove From Favorites</Button>
+  }
+
+  const CardPanel = props => {
+    const { imageSrc, children } = props;
+    return (
+      <Card raised>
+        <Card.Content>
+          <Image
+            rounded
+            fluid
+            size="large"
+            src={imageSrc}
+          />
+        </Card.Content>
+        {children}
+      </Card>
+    )
+  }
+
+  return (
+    <Card.Group itemsPerRow={4}>
+      {results.map(result => {
+        return (
+          <CardPanel imageSrc={result.url}>
+            <Card.Content extra>
+              {
+                favorites.includes(result.id)
+                  ? <RemoveFromFavorites identifier={result.id} />
+                  : <AddToFavorites identifier={result.id} />
+              }
+            </Card.Content>
+          </CardPanel>
+        )
+      })}
+    </Card.Group>
+  )
+}
+
 const Dashboard = () => {
-  const [state, setState] = useState({ currentPage: 0, searchString: "", favorites: [] })
+  const [state, setState] = useState({ currentPage: 0, searchString: "", favorites: [], results: [] })
 
   const handleSearch = () => {
     console.log(state.searchString)
@@ -45,6 +106,10 @@ const Dashboard = () => {
 
   const handleInputChange = (e) => {
     setState({...state, searchString: e.target.value})
+  }
+
+  const handleFavoritesChange = (list) => {
+    setState({...state, favorites: list})
   }
 
   return (
@@ -57,6 +122,11 @@ const Dashboard = () => {
       />
 
       {/*  */}
+      <ListImages
+        results={state.results}
+        favorites={state.favorites}
+        updateFavorites={handleFavoritesChange}
+      />
 
       {/* Pagination */}
 
